7dc5f933aa093176be1d71b5c88676c1d63ddda1
SergeyShapkarin <sergey.shapkarin@mail.ru>
2021-03-05 03:46:11 +0600

    add: what-season.js

--- src/test.js
+++
@@ -1,8 +0,0 @@
-function createDreamTeam(members) {
-    if (members) {
-      members.forEach((item) => item.isString()? console.log(1) : console.log(2))
-    }
-  };
-
-  createDreamTeam([Mamba, Mymra, 123]);
-  
