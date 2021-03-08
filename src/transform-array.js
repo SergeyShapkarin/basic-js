5392bf8d472f349ff682c299704467aa70864c78
SergeyShapkarin <sergey.shapkarin@mail.ru>
2021-03-09 00:27:15 +0600

    fix: transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,42 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  let discard = 0
-  let response = []
-  if (arr.length == 0) {
-      return arr } else {
-          if (Array.isArray(arr)) {
-              arr.forEach((item, index) => {
-                  if (item === '--discard-prev') {
-                      if (index != 0) {
-                          response.splice(index-1, 1)
-                          discard =0
-                      }
-                  } else if (item === '--double-prev') {
-                      if (index != 0) {
-                          if (discard === 2) {
-                              discard = 0
-                          } else {
-                              response.push(response[index-1])
-                          }
-                      }
-                  } else if (item === '--double-next') {
-                      if (index != arr.length-1) {
-                          response.push(arr[index+1])
-                      }
-                  } else if (item === '--discard-next') {
-                      if (index != arr.length-1) {
-                          discard = 1
-                      }
-                  } else {
-                      if (discard === 0) {
-                          response.push(item)
-                      } else { discard = 2 }
-                  }
-              })
-          } else {
-            throw new Error('NA')
-          }
-      }
-return response
+    if (!(arr instanceof Array)) throw new Error();
+    let retValue = [];
+    const discardedIdx = [];
+    for (let i = 0; i < arr.length; ++i) {
+        if (arr[i] == '--discard-next') {
+            discardedIdx.push(++i);
+        } else if (arr[i] == '--discard-prev') {
+            if (retValue.length > 0 && !discardedIdx.some((x) => x === i - 1)) {
+                retValue = retValue.slice(0, retValue.length - 1);
+                discardedIdx.push(i - 1);
+            }
+        } else if (arr[i] == '--double-next') {
+            if (++i < arr.length) {
+                retValue.push(arr[i]);
+                retValue.push(arr[i]);
+            }
+        } else if (arr[i] == '--double-prev') {
+            if (i > 0 && !discardedIdx.some((x) => x === i - 1))
+                retValue.push(arr[i - 1]);
+        } else {
+            retValue.push(arr[i]);
+        }
+    }
+    return retValue;
 };
