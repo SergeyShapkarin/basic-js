ddcb931b48775bc649ee1ba0d685595ae5b4eb53
SergeyShapkarin <sergey.shapkarin@mail.ru>
2021-03-22 17:51:52 +0600

    feat: carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  try {
+    if (sampleActivity && typeof sampleActivity == 'string') {
+      numericValue = parseFloat(sampleActivity);
+      if (numericValue > 0 && numericValue < MODERN_ACTIVITY) {
+        return Math.ceil(Math.log(MODERN_ACTIVITY / numericValue) * HALF_LIFE_PERIOD / 0.693);
+      }
+    }
+  } catch (ex) {}
+  return false;
 };
